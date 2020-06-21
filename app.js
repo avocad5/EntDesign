e2c0e80c576e9a6effdb10239befba44f1a59bbb
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-06-21 19:26:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ function add(a,b,c,d,e){
 
 add('https://playentry.org/uploads/discuss/pa/ck/image/packw2uskaen8yuq00105ac394a2mljx.png','스선로고','avocad5','미리캔버스에서 만든 스선로고입니다.','출처 : designed by Good Ware from flaticon 이라고 남겨주시면 엔트리안에서 쓰실수있습니다.')
 add('https://playentry.org/uploads/discuss/cv/r3/image/cvr3biirkaene484000p5ac394e2fm8b.png','인작로고','avocad5','미리캔버스에서 만든 인작로고입니다.','출처 : designed by FreePik from flaticon 이라고 남겨주시면 엔트리안에서 쓰실수있습니다.')
+add('https://playentry.org/uploads/discuss/vu/ub/image/vuub54o4k9xalxob00105ac3949m4tnk.png','인작로고','njy08192','그냥 평범한 인작,스선 로고임미다','작품 설명란에 제작자 아이디 남겨주세요')
+add('https://playentry.org/uploads/discuss/g8/lv/image/g8lv7vjik9xam4gb00115ac394ai03xs.png','스선로고','njy08192','그냥 평범한 인작,스선 로고임미다','작품 설명란에 제작자 아이디 남겨주세요')
 
 
 //여기부터는 html을 생성하는 코드
